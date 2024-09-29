2633df9a380723c1bef432f9b01b4f4b5ef8707c
kumarvivek <kumar.vivek092002@gmail.com>
2024-09-29 22:16:33 +0530

    files added to arrays

--- arrays/gfg/equilibriumPoint.java
+++ arrays/gfg/equilibriumPoint.java
@@ -24,5 +24,21 @@ public class equilibriumPoint {
         return false;
     }
 
+    //Efficient Solution
+    public static int pivot(int[] nums) {
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        int left_total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int right_total = total -left_total - nums[i];
+            if (right_total == left_total){
+                return i;
+            }
+            left_total += nums[i];
+        }
+        return -1;
+    }
 
 }
--- arrays/leetcode/pivotIndex.java
+++ arrays/leetcode/pivotIndex.java
@@ -1,15 +1,25 @@
 package com.DSA.arrays.leetcode;
-
+//https://leetcode.com/problems/find-pivot-index/description/
 public class pivotIndex {
     public static void main(String[] args) {
-
+        int[] nums = {1,7,3,6,5,6};
+        System.out.println(pivot(nums));
     }
 
-    public int pivot(int[] nums) {
+    //O(N)
+    public static int pivot(int[] nums) {
         int total = 0;
         for (int i = 0; i < nums.length; i++) {
             total += nums[i];
         }
-        
+        int left_total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int right_total = total -left_total - nums[i];
+            if (right_total == left_total){
+                return i;
+            }
+            left_total += nums[i];
+        }
+        return -1;
     }
 }
