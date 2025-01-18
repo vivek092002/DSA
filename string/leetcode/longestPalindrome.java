3581539053b19c2f570d013275524cae8d58a5d2
VIVEK KUMAR <103557229+vivek092002@users.noreply.github.com>
2025-01-18 13:14:22 +0530

    Add files via upload

---
+++ string/leetcode/longestPalindrome.java
@@ -0,0 +1,46 @@
+package com.DSA.string.leetcode;
+//https://leetcode.com/problems/longest-palindromic-substring/
+import java.util.Scanner;
+
+public class longestPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        System.out.println(longestPalin(str));
+        sc.close();
+    }
+
+    public static String longestPalin(String s){
+        if (s.length() <= 1){
+            return s;
+        }
+        int maxLen = 1;
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxLen; j <= s.length(); j++) {
+                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
+                    maxLen = j - i;
+                    maxStr = s.substring(i, j);
+                }
+            }
+        }
+
+        return maxStr;
+    }
+    private static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
